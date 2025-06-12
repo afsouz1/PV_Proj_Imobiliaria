64642194dab45702db52d848111162e8312bbd8c
alan <afsouz.dev@aol.com>
2025-06-11 21:48:59 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import view.ImobiliariaView;
+
+public class Main {
+    public static void main(String[] args) {
+        ImobiliariaView view = new ImobiliariaView();
+        view.iniciar();
+    }
+}
